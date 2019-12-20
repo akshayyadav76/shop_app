b015396f2598c522deb5bbbdb6ee3d8c744daefc
akshay yadav <akshayyadav7613@gmail.com>
2019-12-20 16:29:08 +0530

    finish

--- lib/models/http_exception.dart
+++ lib/models/http_exception.dart
@@ -13,4 +13,3 @@ class HttpException implements Exception{
   }
 }
 
-// comite
--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -1,50 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-
 import '../providers/orders.dart' show Order;
 import '../widgets/order_items.dart';
 import '../widgets/app_drawer.dart';
 
+class OrderScreen extends StatelessWidget {
+  static const routeName = "/orderscreen";
 
-
-
-class OrderScreen  extends StatefulWidget {
-  static const routeName ="/orderscreen";
-
-  @override
-  _OrderScreenState createState() => _OrderScreenState();
-}
-
-
-
-class _OrderScreenState extends State<OrderScreen> {
-
-  var isLoading =false;
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_)async{
-      setState(() {
-        isLoading = true;
-      });
-      await Provider.of<Order>(context,listen: false).fetchSetOrders();
-      setState(() {
-        isLoading =false;
-      });
-    });
-    super.initState();
-  }
   @override
   Widget build(BuildContext context) {
-    final orderData=Provider.of<Order>(context);
+    print("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+    //final orderData = Provider.of<Order>(context);
     return Scaffold(
-      appBar: AppBar(title: Text("your orders"),
-
+      appBar: AppBar(
+        title: Text("your orders"),
       ),
-      body: ListView.builder(itemCount: orderData.orders.length,itemBuilder: (cont,i){
-        return isLoading ? Center(child: CircularProgressIndicator(),):OrderItem(orderData.orders[i]);
-      },),
+      body:FutureBuilder(future: Provider.of<Order>(context,listen: false).fetchSetOrders(),
+        builder: (context,snapshot){
+        if(snapshot.connectionState == ConnectionState.waiting){
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }else if(snapshot.error !=null){
+          //return somethibg
+        }else{
+          return Consumer<Order>(builder: (cont,orderData,child){
+            return ListView.builder(
+              itemCount: orderData.orders.length,
+              itemBuilder: (cont, i) {
+                return OrderItem(orderData.orders[i]);
+              },
+            );
+          },);
+        }
+        return null;
+        },
+      ),
       drawer: AppDrawer(),
     );
   }
+
+
+
 }
